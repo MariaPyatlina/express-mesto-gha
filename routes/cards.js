d87c159f951267c45209e42c86223d02aff9e1a8
LD\mpavlovna <mpavlovna@yandex-team.ru>
2023-02-02 02:33:59 +0300

    PR14: test. remove auth for delete and like

--- routes/cards.js
+++ routes/cards.js
@@ -18,18 +18,18 @@ router.delete('/:cardId', celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().alphanum().length(24),
   }),
-}), auth, deleteCard);
+}), deleteCard);
 
 router.put('/:cardId/likes', celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().alphanum().length(24),
   }),
-}), auth, setLikeCard);
+}), setLikeCard);
 
 router.delete('/:cardId/likes', celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().alphanum().length(24),
   }),
-}), auth, removeLikeCard);
+}), removeLikeCard);
 
 module.exports = router;
