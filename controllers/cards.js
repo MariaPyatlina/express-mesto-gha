be4dd102ec0d82a5130a22bfc653226fa4a210b3
LD\mpavlovna <mpavlovna@yandex-team.ru>
2023-02-02 04:25:04 +0300

    PR14 fix linter recomendation

--- controllers/cards.js
+++ controllers/cards.js
@@ -36,16 +36,16 @@ function getAllCards(req, res, next) {
 }
 
 function deleteCard(req, res, next) {
-
-  Card.findById(req.params.cardId)
+  Card.findByIdAndDelete(req.params.cardId)
     .then((card) => {
       if (!card) {
         return next(new NotFoundError(CARD_NOT_FOUND_ERROR_MSG));
       }
+      // eslint-disable-next-line eqeqeq
       if (card.owner != req.user._id) {
         return next(new ForbiddenError(`${FORBIDDEN_ERROR_MSG} Нельзя удалить чужую карточку`));
       }
-      card.delete(); res.status(200).send({ data: card });
+      return res.status(200).send({ data: card });
     })
     .catch((err) => {
       if (err.name === 'CastError') {
